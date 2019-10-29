6ec8e0dc0bb7363f103a2e31474a1cd326936175
Michaël Zasso <targos@protonmail.com>
2019-10-29 08:28:28 +0100

    tools: enable backtrace when V8 is built for PPC
    Refs: https://github.com/nodejs/node-v8/issues/119

--- tools/make-v8.sh
+++ tools/make-v8.sh
@@ -32,7 +32,7 @@ elif [[ "$ARCH" == "ppc64le" ]]; then
   fi
   g++ --version
   export PKG_CONFIG_PATH=$BUILD_TOOLS/pkg-config-files
-  gn gen out.gn/$BUILD_ARCH_TYPE --args='is_component_build=false is_debug=false use_goma=false goma_dir="None" use_custom_libcxx=false v8_target_cpu="ppc64" target_cpu="ppc64"'
+  gn gen out.gn/$BUILD_ARCH_TYPE --args='is_component_build=false is_debug=false use_goma=false goma_dir="None" use_custom_libcxx=false v8_target_cpu="ppc64" target_cpu="ppc64" v8_enable_backtrace=true'
   ninja -C out.gn/$BUILD_ARCH_TYPE d8 cctest inspector-test
 else
   PATH=~/_depot_tools:$PATH tools/dev/v8gen.py $BUILD_ARCH_TYPE --no-goma $V8_BUILD_OPTIONS
